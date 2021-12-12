23d0442e1f1626c5794e6e7cc5fbb9fe437bb2ce
thanh2 <pct1003@gmail.com>
2021-12-12 22:07:03 +0700

    4th

--- src/config/server.js
+++ src/config/server.js
@@ -31,6 +31,6 @@ app.get('', (req,res) => {
   res.send('Verified');
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('server is running in port 5000')
 })
