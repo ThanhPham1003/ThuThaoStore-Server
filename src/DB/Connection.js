6bf8dac112d68fe5ca3c71c27c92ad7a7d61d8f2
thanh2 <pct1003@gmail.com>
2021-12-12 22:20:17 +0700

    5th

--- src/DB/Connection.js
+++ src/DB/Connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const URI = 'mongodb+srv://chithanh:chithanh123@cluster0.pciov.mongodb.net/myFirstDatabase?retryWrites=true&w=majoritymongodb+srv://chithanh:chithanh123@cluster0.pciov.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 const connectDB = async () => {
-  await mongoose.connect(URI, {
+  await mongoose.connect(process.env.MONGODB_URI || URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   });
