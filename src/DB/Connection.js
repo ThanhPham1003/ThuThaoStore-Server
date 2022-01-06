c5317154010df9de7cdfdcc69aacae436d657064
thanh2 <pct1003@gmail.com>
2022-01-06 11:12:00 +0700

    test environment

--- src/DB/Connection.js
+++ src/DB/Connection.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const URI = 'mongodb+srv://chithanh:chithanh123@cluster0.pciov.mongodb.net/myFirstDatabase?retryWrites=true&w=majority;'
 
 const connectDB = async () => {
+  console.log('---- process.env.MONGODB_URI --- ', process.env.MONGODB_URI);
   await mongoose.connect(process.env.MONGODB_URI || URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
